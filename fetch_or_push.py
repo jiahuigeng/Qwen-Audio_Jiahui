7fa762e4a9a258d279e8541c0192cecffd2e95c0
jiahuigeng <jiahui.geng@rwth-aachen.de>
2024-06-23 12:55:34 +0400

    copy full lib

--- fetch_or_push.py
+++ fetch_or_push.py
@@ -8,13 +8,16 @@ from pathlib import Path
 def operate(args):
     home_directory = Path.home()
     tgt_path = os.path.join(home_directory, ".cache","huggingface","modules","transformers_modules","Qwen","Qwen-Audio-Chat","a52fffad1964a463791eb6a10d354b6de31a069d")
-    loc_path = os.path.join("./")
-    files = ["modeling_qwen.py", "tokenization_qwen.py"]
+    loc_path = os.path.join("local_lib")
+    if not os.path.exists(loc_path):
+        os.makedirs(loc_path)
 
     exists = os.path.exists(tgt_path)
     print(f"tgt path exsits: {exists}")
 
     if exists:
+        files = os.listdir(tgt_path)
+        files = [file for file in files if file.endswith(".py")]
         if args.op == "fetch":
             for file in files:
                 copyfile(os.path.join(tgt_path,file), os.path.join(loc_path,file))
@@ -22,6 +25,7 @@ def operate(args):
         elif args.op == "push":
             for file in files:
                 copyfile(os.path.join(loc_path, file), os.path.join(tgt_path,file))
+
 
 if __name__ == '__main__':
 
